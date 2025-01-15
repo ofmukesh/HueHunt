9526eb4bda59712612f9940f64ef66c5905b929c
Mukesh <of.mukesh@gmail.com>
2025-01-15 23:00:39 +0530

    maefile chnages

--- src/HueHunt/urls.py
+++ src/HueHunt/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
 
 handler404 = 'HueHunt.views.custom_404'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                         document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
